a38326e9bc0dbc56a617edf09d892ae287125613
Tashini Monasha <tashinimonasha44@gmail.com>
2024-09-13 23:28:34 +0530

    change css and others

--- login.php
+++ login.php
@@ -16,7 +16,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         $_SESSION['user_token'] = bin2hex(random_bytes(32)); // session token
         $_SESSION['user_id'] = $user['id'];
         $_SESSION['user_role'] = $user['role'];
-        header("Location: dashboard.php");
+        header("Location: dashboard\\.php");
         exit;
     } else {
         $error = "Invalid username or password";
